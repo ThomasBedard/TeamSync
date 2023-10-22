97c345777b11ca0ff9acbb88b52c8f4429996d34
Thomas <thomas.bedard0@gmail.com>
2023-10-22 11:37:03 -0400

    Initial Commit

---
+++ TeamSync/TeamSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamSyncApp.swift
+//  TeamSync
+//
+//  Created by Thomas Bedard on 2023-10-22.
+//
+
+import SwiftUI
+
+@main
+struct TeamSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
